dffef4128e997f70f474091a125c740757782bde
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-02-21 23:16:51 -0800

    implement working alg in proper runtime

--- dead_ends.py
+++ dead_ends.py
@@ -1,10 +1,9 @@
 import csv
-from tarjan import tarjan
 
-# TODO: fix reading input file 
+# TODO: fix reading input file
 
 def write_to_output_file(output):
-  output_file = open('./outputs/deadends_sample_output.tsv','w+')
+  output_file = open('./outputs/deadends_10k.tsv','w+')
   for val in output:
     output_file.write('{}\n'.format(val))
 
@@ -22,40 +21,56 @@ def generate_adjacency_list(edge_u, edge_v, adj_list_dict):
   for i in range(len(edge_u)):
     u = edge_u[i]
     v = edge_v[i]
-    adj_list_dict[u].append(v)
+    adj_list_dict[u].add(v)
 
   return adj_list_dict
 
 
-with open('./sample_input.txt') as inf:
+def generate_reverse_hash_map_and_auto_dead_ends(adj_list_dict):
+  reverse_hash_map = { k: set() for k in nodes_list }
+  automatic_dead_ends_1 = set()
+  automatic_dead_ends_2 = set()
+
+  for key, value in adj_list_dict.items():
+    if (len(value) == 0):
+      automatic_dead_ends_1.add(key)
+      automatic_dead_ends_2.add(key)
+    else:
+      for val in value:
+        if (reverse_hash_map.get(val) is None):
+          reverse_hash_map[val] = set(key)
+        else:
+          reverse_hash_map[val].add(key)
+
+  return reverse_hash_map, automatic_dead_ends_1, automatic_dead_ends_2
+
+
+with open('./web-Google_800k.txt') as inf:
   reader = csv.reader(inf, delimiter='\t')
   node_list = zip(*list(reader))
   nodes = list(node_list)
 
   edge_u, edge_v = generate_edge_list(nodes)
+  nodes_list = list(set(edge_u) | set(edge_v))
 
-  # number of nodes
-  n = 6
-  adj_list_dict = { k: [] for k in edge_u }
+  adj_list_dict = { k: set() for k in nodes_list }
   adj_list_dict = generate_adjacency_list(edge_u, edge_v, adj_list_dict)
 
-  # print("Adjacency list: ")
-  print(adj_list_dict)
+  reverse_hash_map, automatic_dead_ends_1, automatic_dead_ends_2 = generate_reverse_hash_map_and_auto_dead_ends(adj_list_dict)
 
-  false_dead_ends = []
+  while (len(automatic_dead_ends_2) != 0):
+    dead_end = automatic_dead_ends_2.pop()
+    automatic_dead_ends_2.add(dead_end)
+    parents = reverse_hash_map[dead_end] - automatic_dead_ends_1
 
-  for key, value in adj_list_dict.items():
-    if (key in adj_list_dict.get(key)):
-      false_dead_ends.append(key)
+    for value in list(parents):
+      if (len(adj_list_dict[value] - automatic_dead_ends_1) is 0):
+        automatic_dead_ends_1.add(value)
+        automatic_dead_ends_2.add(value)
 
-  print(false_dead_ends)
-  scc = tarjan(adj_list_dict)
-  print(scc)
+    automatic_dead_ends_2.remove(dead_end)
 
-  final_dead_end_list = []
+  # print(automatic_dead_ends_2)
+  print(len(automatic_dead_ends_1))
 
-  for component in scc:
-    if (len(component) == 1 and component[0] not in false_dead_ends):
-      final_dead_end_list.append(component[0])
-
-  write_to_output_file(final_dead_end_list)
+  # write_to_output_file(final_dead_end_list)
