62af25e104662e78da3a14cfaebe977feb9363b2
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-03-03 11:52:58 -0800

    fix runtime and perform refactoring

--- page_rank.py
+++ page_rank.py
@@ -1,81 +1,7 @@
 import csv
 import argparse
 import numpy as np
-import copy
-import time
-
-def constructArguments():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-i', '--input', type=str, default=None,
-    help='input path of data file')
-  parser.add_argument('-o', '--output', type=str, default=None,
-    help='output path of data file')
-  args = vars(parser.parse_args())
-
-  return args
-
-
-def generate_edge_list(nodes):
-  edge_u = list(nodes[0])
-  edge_v = list(nodes[1])
-  edge_u = [int(x) for x in edge_u]
-  edge_v = [int(y) for y in edge_v]
-
-  return edge_u, edge_v
-
-
-def generate_adjacency_list(edge_u, edge_v, nodes_list):
-  adj_list_dict = { k: set() for k in nodes_list }
-  dup_adj_list_dict = { k: set() for k in nodes_list }
-
-  for i in range(len(edge_u)):
-    u = edge_u[i]
-    v = edge_v[i]
-    adj_list_dict[u].add(v)
-    dup_adj_list_dict[u].add(v)
-
-  return adj_list_dict, dup_adj_list_dict
-
-
-def generate_reverse_hash_map_and_auto_dead_ends(adj_list_dict, nodes_list):
-  reverse_hash_map = { k: set() for k in nodes_list }
-  dup_reverse_hash_map = { k: set() for k in nodes_list }
-  automatic_dead_ends_1 = set()
-  automatic_dead_ends_2 = set()
-
-  for key, value in adj_list_dict.items():
-    if (len(value) == 0):
-      automatic_dead_ends_1.add(key)
-      automatic_dead_ends_2.add(key)
-    else:
-      for val in value:
-        if (reverse_hash_map.get(val) is None):
-          reverse_hash_map[val] = set(key)
-        else:
-          reverse_hash_map[val].add(key)
-        if (dup_reverse_hash_map.get(val) is None):
-          dup_reverse_hash_map[val] = set(key)
-        else:
-          dup_reverse_hash_map[val].add(key)
-
-  return reverse_hash_map, dup_reverse_hash_map, automatic_dead_ends_1, automatic_dead_ends_2
-
-
-def find_parent_dead_ends(reverse_hash_map, automatic_dead_ends_1, automatic_dead_ends_2):
-  while (len(automatic_dead_ends_2) != 0):
-    dead_end = automatic_dead_ends_2.pop()
-    automatic_dead_ends_2.add(dead_end)
-    parents = reverse_hash_map[dead_end] - automatic_dead_ends_1
-
-    for value in list(parents):
-      if (len(adj_list_dict[value] - automatic_dead_ends_1) is 0):
-        automatic_dead_ends_1.add(value)
-        automatic_dead_ends_2.add(value)
-
-    automatic_dead_ends_2.remove(dead_end)
-
-  return automatic_dead_ends_1
-
+from .utils import constructArguments, generate_adjacency_lists, generate_automatic_dead_ends, find_parent_dead_ends
 
 def remove_dead_ends(adj_list_dict_without_dead_ends, reverse_hash_map_without_dead_ends, all_dead_ends):
   removed_dead_ends = []
@@ -129,15 +55,11 @@ with open(args['input'], 'r') as inf:
   for i in range(4):
     next(reader, None)
 
-  node_list = zip(*list(reader))
-  nodes = list(node_list)
+  int_reader_list = [[int(j) for j in i] for i in list(reader)]
 
-  edge_u, edge_v = generate_edge_list(nodes)
-  nodes_set = set(edge_u) | set(edge_v)
-  nodes_list = list(nodes_set)
-
-  adj_list_dict, dup_adj_list_dict = generate_adjacency_list(edge_u, edge_v, nodes_list)
-  reverse_hash_map, dup_reverse_hash_map, automatic_dead_ends_1, automatic_dead_ends_2 = generate_reverse_hash_map_and_auto_dead_ends(adj_list_dict, nodes_list)  
+  adj_list_dict, dup_adj_list_dict, reverse_hash_map, dup_reverse_hash_map = generate_adjacency_lists(int_reader_list)
+  nodes_set = set(adj_list_dict.keys())
+  automatic_dead_ends_1, automatic_dead_ends_2 = generate_automatic_dead_ends(adj_list_dict)
   all_dead_ends = find_parent_dead_ends(reverse_hash_map, automatic_dead_ends_1, automatic_dead_ends_2)
   removed_dead_ends, adj_list_dict_without_dead_ends, reverse_hash_map_without_dead_ends = remove_dead_ends(dup_adj_list_dict, dup_reverse_hash_map, all_dead_ends)  
   page_rank_without_dead_ends = page_rank_without_dead_ends(adj_list_dict_without_dead_ends, nodes_set, reverse_hash_map_without_dead_ends, all_dead_ends)
---
+++ utils.py
@@ -0,0 +1,78 @@
+import csv
+import argparse
+import numpy as np
+
+def constructArguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', '--input', type=str, default=None,
+    help='input path of data file')
+  parser.add_argument('-o', '--output', type=str, default=None,
+    help='output path of data file')
+  args = vars(parser.parse_args())
+
+  return args
+
+def generate_adjacency_lists(int_reader_list):
+  adj_list_dict = {}
+  dup_adj_list_dict = {}
+  reverse_hash_map = {}
+  dup_reverse_hash_map = {}
+
+  for pair in int_reader_list:
+    node = pair[0]
+    child = pair[1]
+
+    if (node not in adj_list_dict):
+      adj_list_dict[node] = set()
+      dup_adj_list_dict[node] = set()
+    if (child not in adj_list_dict):
+      adj_list_dict[child] = set()
+      dup_adj_list_dict[child] = set()
+    if (node not in reverse_hash_map):
+      reverse_hash_map[node] = set()
+      dup_reverse_hash_map[node] = set()
+    if (child not in reverse_hash_map):
+      reverse_hash_map[child] = set()
+      dup_reverse_hash_map[child] = set()
+
+    if (node in adj_list_dict):
+      adj_list_dict[node].add(child)
+      dup_adj_list_dict[node].add(child)
+    else:
+      adj_list_dict[node] = set([child])
+      dup_adj_list_dict[node] = set([child])
+    if (child in reverse_hash_map):
+      reverse_hash_map[child].add(node)
+      dup_reverse_hash_map[child].add(node)
+    else:
+      reverse_hash_map[child] = set([node])
+      dup_reverse_hash_map[child] = set([node])
+
+  return adj_list_dict, dup_adj_list_dict, reverse_hash_map, dup_reverse_hash_map
+
+def generate_automatic_dead_ends(adj_list_dict):
+  automatic_dead_ends_1 = set()
+  automatic_dead_ends_2 = set()
+
+  for key, value in adj_list_dict.items():
+    if (len(value) == 0):
+      automatic_dead_ends_1.add(key)
+      automatic_dead_ends_2.add(key)
+
+  return automatic_dead_ends_1, automatic_dead_ends_2
+
+
+def find_parent_dead_ends(reverse_hash_map, automatic_dead_ends_1, automatic_dead_ends_2):
+  while (len(automatic_dead_ends_2) != 0):
+    dead_end = automatic_dead_ends_2.pop()
+    automatic_dead_ends_2.add(dead_end)
+    parents = reverse_hash_map[dead_end] - automatic_dead_ends_1
+
+    for value in list(parents):
+      if (len(adj_list_dict[value] - automatic_dead_ends_1) is 0):
+        automatic_dead_ends_1.add(value)
+        automatic_dead_ends_2.add(value)
+
+    automatic_dead_ends_2.remove(dead_end)
+
+  return automatic_dead_ends_1
